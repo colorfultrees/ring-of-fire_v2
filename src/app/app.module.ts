eb8432b82a4314df3b555b6a013f9088f5441fbd
colorfultrees <colorfultrees@users.noreply.github.com>
2023-02-26 12:30:38 +0100

    firebase implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CardRuleComponent } from './card-rule/card-rule.component';
 import { MatCardModule } from '@angular/material/card';
-import { DialogGameoverComponent } from './dialog-gameover/dialog-gameover.component'; 
+import { DialogGameoverComponent } from './dialog-gameover/dialog-gameover.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore'; 
 
 @NgModule({
   declarations: [
@@ -37,7 +42,11 @@ import { DialogGameoverComponent } from './dialog-gameover/dialog-gameover.compo
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-96dbe',
+    appId: '1:916101140125:web:2dd277858b1b8ec0ef4e74',
+    storageBucket: 'ring-of-fire-96dbe.appspot.com',
+    apiKey: 'AIzaSyCIwjJsY75Px4-qQbe52UWGhU-fMMYEUS8',
+    authDomain: 'ring-of-fire-96dbe.firebaseapp.com',
+    messagingSenderId: '916101140125',
+  },};
